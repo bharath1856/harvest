3f41acb218686e9a30ed65afb73186fcd17a4b28
bharath1856 <pvrbharath2006@gmail.com>
2026-07-01 15:57:34 +0530

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bb0e133a2576db5a"; }
+/******/ 		__webpack_require__.h = function() { return "470b91ff1d70b48d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
